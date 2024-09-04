6549119239b69999586acf58b8e51d402c16ab36
kimkim1105 <huenk105@gmail.com>
2024-09-04 08:19:15 +0700

    init

---
+++ src/main/java/com/kimsomin/nuts/NutsApplication.java
@@ -0,0 +1,13 @@
+package com.kimsomin.nuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimsomin/nuts/NutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimsomin.nuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
